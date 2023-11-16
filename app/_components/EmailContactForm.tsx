53cfdf4eb9200ea95d6f4560e6fe3762e3c9f4c9
marlin dalpozzo <marlin.dalpozzo@gmail.com>
2023-11-16 08:38:41 -0800

    adjust project section layout. add loading animation and feedback to contact form.

--- app/_components/EmailContactForm.tsx
+++ app/_components/EmailContactForm.tsx
@@ -1,8 +1,16 @@
 'use client'
 
-import { FormEventHandler, useState } from "react"
+import { FormEventHandler, forwardRef, useState } from "react"
 import { EmailRequestBody } from "../_types/ContactSendEmail"
-import { DividerHorizontal } from "./DividerHorizontal"
+import { LinearProgress, Snackbar } from "@mui/material"
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+
+const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export const EmailContactForm = () => {
     const [name, setName] = useState('')
@@ -10,8 +18,22 @@ export const EmailContactForm = () => {
     const [subject, setSubject] = useState('')
     const [message, setMessage] = useState('')
 
+    const [isSending, setIsSending] = useState(false)
+    const [sendSuccess, setSendSuccess] = useState(false)
+    const [sendError, setSendError] = useState(false)
+    const [snackbarOpen, setSnackbarOpen] = useState(false)
+
+    const clearAllFields = () => {
+        setName('')
+        setEmail('')
+        setSubject('')
+        setMessage('')
+    }
+
     const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
         event.preventDefault();
+
+        setIsSending(true)
 
         const body: EmailRequestBody = { name, email, subject, body: message }
 
@@ -26,13 +48,21 @@ export const EmailContactForm = () => {
         // TODO - show snackbar or something change ui
         if (response.ok) {
             // Handle success - e.g., show a success message
+            setSendSuccess(true)
+            setSendError(false)
+            clearAllFields()
         } else {
             // Handle error - e.g., show an error message
+            setSendSuccess(false)
+            setSendError(true)
         }
+
+        setSnackbarOpen(true)
+        setIsSending(false)
     };
 
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col">
+        <form onSubmit={handleSubmit} className="flex flex-col relative">
             <label htmlFor="name">Name</label>
             <input
                 className="contact-input"
@@ -42,9 +72,8 @@ export const EmailContactForm = () => {
                 onChange={(e) => setName(e.target.value)}
                 placeholder="Your name"
                 required
+                disabled={isSending}
             />
-
-            {/* <DividerHorizontal className="my-2" /> */}
 
             <label htmlFor="email">Email</label>
             <input
@@ -55,9 +84,8 @@ export const EmailContactForm = () => {
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="Your email"
                 required
+                disabled={isSending}
             />
-
-            {/* <DividerHorizontal className="my-2" /> */}
 
             <label htmlFor="subject">Subject</label>
             <input
@@ -68,9 +96,8 @@ export const EmailContactForm = () => {
                 onChange={(e) => setSubject(e.target.value)}
                 placeholder="Email subject/title"
                 required
+                disabled={isSending}
             />
-
-            {/* <DividerHorizontal className="my-2" /> */}
 
             <label htmlFor="message">Message</label>
             <textarea
@@ -80,9 +107,28 @@ export const EmailContactForm = () => {
                 onChange={(e) => setMessage(e.target.value)}
                 placeholder="Email message"
                 required
+                disabled={isSending}
             />
 
-            <button type="submit" className="p-2 bg-slate-500 mt-4 rounded-md hover:bg-slate-600">Send email to me</button>
+            <button disabled={isSending} type="submit" className="p-2 bg-slate-500 mt-4 rounded-md hover:bg-slate-600">Send email to me</button>
+
+
+            {isSending && <div className="absolute rounded-md top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-purple-400 bg-opacity-50 p-6">
+                <LinearProgress className="w-full h-10 rounded-md" />
+            </div>}
+
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={6000}
+            >
+                <Alert
+                    onClose={() => setSnackbarOpen(false)}
+                    severity={sendSuccess ? "success" : "error"}
+                    sx={{ width: '100%' }}
+                >
+                    {sendSuccess ? 'Email sent successfully!' : 'Error sending email'}
+                </Alert>
+            </Snackbar>
         </form>
     );
 }
