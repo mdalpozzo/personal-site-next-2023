2d7ee380af55155dbc15e39f8cf73f36bd2291ca
Marlin Dalpozzo <mdalpozzo@Marlins-MacBook-Pro.local>
2023-11-20 20:17:01 -0800

    implement contact page with email form and social links.

--- app/_components/DividerHorizontal.tsx
+++ app/_components/DividerHorizontal.tsx
@@ -1,3 +1,11 @@
-export const DividerHorizontal = () => {
-  return <div className="h-[1px] w-full my-10 bg-zinc-400" />
+import { cn } from '@utils/cn'
+
+interface DividerHorizontalProps {
+  className?: string
+}
+
+export const DividerHorizontal = ({
+  className
+}: DividerHorizontalProps) => {
+  return <div className={cn("h-[1px] w-full my-10 bg-zinc-400", className)} />
 }
--- app/page.tsx
+++ app/page.tsx
@@ -13,128 +13,128 @@ import { SectionTitle } from './_components/SectionTitle'
 const ICON_SIZE = 30
 
 export default function Page() {
-  return (
-    <section className="w-full">
-      <div className="min-h-[240px] h-[40vh] md:h-[30vh] w-full flex flex-row gap-6 md:gap-20">
-        <div className="w-1/3 min-w-[120px] h-full relative">
-          <CustomImage
-            src="/images/marlin-portrait-photo.jpeg"
-            alt="Personal Portrait"
-            fill
-            style={{ objectPosition: '50% 30%' }}
-            className="aspect-video object-cover"
-          />
-        </div>
+    return (
+        <section className="w-full">
+            <div className="min-h-[240px] h-[40vh] md:h-[30vh] w-full flex flex-row gap-6 md:gap-20">
+                <div className="w-1/3 min-w-[120px] h-full relative">
+                    <CustomImage
+                        src="/images/marlin-portrait-photo.jpeg"
+                        alt="Personal Portrait"
+                        fill
+                        style={{ objectPosition: '50% 30%' }}
+                        className="aspect-video object-cover"
+                    />
+                </div>
 
-        <div className="w-full flex justify-center">
-          <div className="flex flex-col justify-between">
-            <div className="mb-10">
-              <h1 className="font-bold text-4xl md:text-5xl lg:text-6xl mb-8">
-                marlin dalpozzo
-              </h1>
+                <div className="w-full flex justify-center">
+                    <div className="flex flex-col justify-between">
+                        <div className="mb-10">
+                            <h1 className="font-bold text-4xl md:text-5xl lg:text-6xl mb-8">
+                                marlin dalpozzo
+                            </h1>
 
-              <p className="prose prose-neutral dark:prose-invert text-2xl md:text-3xl">
-                full stack javascript developer
-              </p>
+                            <p className="prose prose-neutral dark:prose-invert text-2xl md:text-3xl">
+                                full stack javascript developer
+                            </p>
+                        </div>
+
+                        <p className="prose prose-neutral dark:prose-invert text-base">
+                            i'm a full stack developer with an emphasis on frontend/UX
+                        </p>
+                    </div>
+                </div>
             </div>
 
-            <p className="prose prose-neutral dark:prose-invert text-base">
-              full stack developer with an emphasis on frontend/UX
-            </p>
-          </div>
-        </div>
-      </div>
+            <DividerHorizontal />
 
-      <DividerHorizontal />
+            <div className="w-full flex flex-col">
+                <p className="text-2xl inline-block mb-8">Languages</p>
 
-      <div className="w-full flex flex-col">
-        <p className="text-2xl inline-block mb-8">Languages</p>
+                <div>
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                        {HomeConfig.languages.map((skill) => {
+                            const { Icon, linkURL, name } = skill
 
-        <div>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-            {HomeConfig.languages.map((skill) => {
-              const { Icon, linkURL, name } = skill
-
-              return (
-                <div className="flex">
-                  <SkillChip key={name} Icon={Icon} src={linkURL} name={name} />
+                            return (
+                                <div className="flex">
+                                    <SkillChip key={name} Icon={Icon} src={linkURL} name={name} />
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-              )
-            })}
-          </div>
-        </div>
 
-        <DividerHorizontal />
+                <DividerHorizontal />
 
-        <p className="text-2xl inline-block mb-6 mt-4 md:mt-10">Frameworks</p>
+                <p className="text-2xl inline-block mb-6 mt-4 md:mt-10">Frameworks</p>
 
-        <SectionTitle name="Web" className="text-xl md:text-2xl mb-8" />
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          {HomeConfig.frameworks.web.map((skill) => {
-            const { Icon, linkURL, name } = skill
+                <SectionTitle name="Web" className="text-xl md:text-2xl mb-8" />
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                    {HomeConfig.frameworks.web.map((skill) => {
+                        const { Icon, linkURL, name } = skill
 
-            return (
-              <div className="flex">
-                <SkillChip key={name} Icon={Icon} src={linkURL} name={name} />
-              </div>
-            )
-          })}
-        </div>
+                        return (
+                            <div className="flex">
+                                <SkillChip key={name} Icon={Icon} src={linkURL} name={name} />
+                            </div>
+                        )
+                    })}
+                </div>
 
-        <SectionTitle
-          name="Mobile"
-          className="text-xl md:text-2xl mb-8 mt-4 md:mt-10"
-        />
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          {HomeConfig.frameworks.mobile.map((skill) => {
-            const { Icon, linkURL, name } = skill
+                <SectionTitle
+                    name="Mobile"
+                    className="text-xl md:text-2xl mb-8 mt-4 md:mt-10"
+                />
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                    {HomeConfig.frameworks.mobile.map((skill) => {
+                        const { Icon, linkURL, name } = skill
 
-            return (
-              <div className="flex">
-                <SkillChip key={name} Icon={Icon} src={linkURL} name={name} />
-              </div>
-            )
-          })}
-        </div>
+                        return (
+                            <div className="flex">
+                                <SkillChip key={name} Icon={Icon} src={linkURL} name={name} />
+                            </div>
+                        )
+                    })}
+                </div>
 
-        <DividerHorizontal />
+                <DividerHorizontal />
 
-        <p className="text-2xl inline-block mb-8 mt-4 md:mt-10">Tools</p>
+                <p className="text-2xl inline-block mb-8 mt-4 md:mt-10">Tools</p>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          {HomeConfig.tools.map((skill) => {
-            const { Icon, linkURL, name } = skill
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                    {HomeConfig.tools.map((skill) => {
+                        const { Icon, linkURL, name } = skill
 
-            return (
-              <div className="flex">
-                <SkillChip key={name} Icon={Icon} src={linkURL} name={name} />
-              </div>
-            )
-          })}
-        </div>
+                        return (
+                            <div className="flex">
+                                <SkillChip key={name} Icon={Icon} src={linkURL} name={name} />
+                            </div>
+                        )
+                    })}
+                </div>
 
-        <DividerHorizontal />
+                <DividerHorizontal />
 
-        <p className="text-2xl inline-block mb-8 mt-4 md:mt-10">Skills</p>
+                <p className="text-2xl inline-block mb-8 mt-4 md:mt-10">Skills</p>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          <div className="flex">
-            <p>SEO</p>
-          </div>
-          <div className="flex">
-            <p>CRO</p>
-          </div>
-          <div className="flex">
-            <p>Affiliate Marketing</p>
-          </div>
-          <div className="flex">
-            <p>E-commerce</p>
-          </div>
-          <div className="flex">
-            <p>Analytics</p>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+                    <div className="flex">
+                        <p>SEO</p>
+                    </div>
+                    <div className="flex">
+                        <p>CRO</p>
+                    </div>
+                    <div className="flex">
+                        <p>Affiliate Marketing</p>
+                    </div>
+                    <div className="flex">
+                        <p>E-commerce</p>
+                    </div>
+                    <div className="flex">
+                        <p>Analytics</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
